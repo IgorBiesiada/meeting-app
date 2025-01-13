365932407e29b80c62afd4edc950283d49709519
Igor <xxxigorbiesiada1@gmail.com>
2025-01-13 18:12:05 +0100

    add models form view urls and register html

--- users/views.py
+++ users/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from .models import User
+from django.views.generic import CreateView
+from users.forms import UserRegistrationForm
+
 
 # Create your views here.
+
+class RegisterUserView(CreateView):
+    model = User
+    form_class = UserRegistrationForm
+    template_name = 'register.html'
+
+    def form_valid(self, form):
+        user = form.save()
+        return user
