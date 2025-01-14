83801063fac41f0945d45a42736d6ab90825d9e3
Igor <xxxigorbiesiada1@gmail.com>
2025-01-14 16:50:47 +0100

    trying to add mail sending

--- config/settings.py
+++ config/settings.py
@@ -13,6 +13,7 @@ import os
 from pathlib import Path
 from dotenv import load_dotenv
 import dj_database_url
+from decouple import config
 
 load_dotenv()
 
@@ -45,6 +46,7 @@ INSTALLED_APPS = [
 ]
 
 AUTH_USER_MODEL = 'users.User'
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -135,3 +137,11 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_HOST = config('EMAIL_HOST', default='smtp.gmail.com')
+EMAIL_PORT = config('EMAIL_PORT', default=587, cast=int)
+EMAIL_USE_TLS = config('EMAIL_USE_TLS', default=True, cast=bool)
+EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
+DEFAULT_FROM_EMAIL = config('DEFAULT_FROM_EMAIL')
--- users/views.py
+++ users/views.py
@@ -1,7 +1,7 @@
 from .models import User
 from django.views.generic import CreateView
 from users.forms import UserRegistrationForm
-
+from django.core.mail import send_mail
 
 # Create your views here.
 
@@ -12,4 +12,11 @@ class RegisterUserView(CreateView):
 
     def form_valid(self, form):
         user = form.save()
+        send_mail(
+            'lest s meet',
+            'Witamy na pokładzie życzymy miłych spotkań',
+            'example@email.com',
+            [User.email],
+            fail_silently=False
+        )
         return super().form_valid(form)
