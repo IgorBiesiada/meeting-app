2080e9db2c048f8cd03a4c9bf137eaf3b06c61c3
Igor <xxxigorbiesiada1@gmail.com>
2025-01-20 02:10:18 +0100

    add meeting models

--- meetings/models.py
+++ meetings/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
+from config import settings
+
+
 # Create your models here.
+
+class Meeting(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    date = models.DateField()
+    time = models.TimeField()
+    created_by = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='meetings_created')
+    created_at = models.DateTimeField(auto_now_add=True)
+    number_of_seats = models.IntegerField(default=0)
+    waiting_people = models.IntegerField(default=0)
+    price = models.DecimalField(max_digits=10, decimal_places=2, blank=True, null=True)
+
